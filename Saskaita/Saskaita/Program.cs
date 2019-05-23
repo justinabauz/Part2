1332dd1c891aba42056bf86b25394b7e4b38319c
Justina <justina.bauziene@gmail.com>
2019-05-23 21:43:38 +0300

    +

--- Saskaita/Saskaita/Program.cs
+++ Saskaita/Saskaita/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using Saskaita_Struct;
 
-namespace Saskaita
+namespace Saskaita_Project
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Saskaita invoice = new Saskaita("Vardas", "siutejass", 568);
+            Console.WriteLine(invoice.Siuntejas);
+            Console.WriteLine(invoice.Gavejas);
+            Console.WriteLine(invoice.MoketiViso);
+
+            Console.WriteLine(invoice.SaskaitosNumeris);
         }
     }
 }
--- Saskaita/Saskaita_Struct/Class1.cs
+++ Saskaita/Saskaita_Struct/Class1.cs
@@ -2,7 +2,60 @@
 
 namespace Saskaita_Struct
 {
-    public class Class1
+    public struct Saskaita
     {
+        public string _gavejas;
+        public string _siuntejas;
+        public int _moketiViso;
+
+
+        public string Gavejas
+        {
+            get
+            {
+                return _gavejas;
+            }
+            private set
+            {
+                _gavejas = value;
+            }
+        }
+       public string Siuntejas
+        {
+            get
+            {
+                return _siuntejas;
+            }
+            private set
+            {
+                _siuntejas = value;
+            }
+        }
+        public int MoketiViso
+        {
+            get
+            {
+                return _moketiViso;
+            }
+            private set
+            {
+                _moketiViso = value;
+            }
+        }
+        public string SaskaitosNumeris
+        {
+            get
+            {
+                return string.Format("Nr_2019-05-12(01)");
+            }
+        }
+
+        public Saskaita(string gavejas, string siuntejas, int moketiViso)
+        {
+            _gavejas = gavejas;
+            _siuntejas = siuntejas;
+            _moketiViso = moketiViso;
+
+        }
     }
 }
